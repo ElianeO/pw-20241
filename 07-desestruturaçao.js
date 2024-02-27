fd2c064f347c15a73c4fb457dd7b40b61f401af5
ElianeO <elianecristina_d@yahoo.com>
2024-02-27 08:22:12 -0300

    (27/02)

--- 07-desestruturaçao.js
+++ 07-desestruturaçao.js
@@ -23,13 +23,38 @@ console.log({h2, h3})
 
 console.log('-'.repeat(60))
 
-/* Problema: troca de valores entre variáveis (swap) */
-let x = 5, y = 10
-console.log('Antes do swap: ', {x, y})
+// /* Problema: troca de valores entre variáveis (swap) */
+// let x = 5, y = 10
+// console.log('Antes do swap: ', {x, y})
 
-/* Swap feito de forma "tradicional", com uso de variável temporária */
-let temp = x
-x = y
-y = temp
+// /* Swap feito de forma "tradicional", com uso de variável temporária */
+// let temp = x
+// x = y
+// y = temp
 
-console.log('Depois do swap: ', {x, y})
+// console.log('Depois do swap: ', {x, y})
+
+// Swap feito por meio de desestruturação
+[x,y] = [y, x];
+
+console.log('Depois do swap: ', {x,y})
+
+console.log('-'.repeat(60))
+
+/*****************************************************************/
+
+// 2) DESESTRUTURAÇÃO DE OBJETOS
+
+// Na desestruturação de objetos, as variáveis avulsas:
+
+// 1) Devem ter o MESMO NOME das propriedades do objetos
+// 2) Podem ser especificificadas em qualquer ordem
+// 3) Pode ser feita a desestruturação parcial
+
+let {sexo, nome, email} = pessoa
+console.log({nome, sexo, email})
+
+
+
+
+
