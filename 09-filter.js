708043121f73f1f6981f35b128cdbd278f02b868
ElianeO <elianecristina_d@yahoo.com>
2024-02-27 08:54:39 -0300

    (27/01)

---
+++ 09-filter.js
@@ -0,0 +1,28 @@
+/* 
+O método filter() cria um NOVO VETOR contendo apenas os elementos do vetor de prigem 
+que atendam ao critério especificado pela função passada comp parâmetro
+*/
+
+// Vetores para busca
+const numeros = [10, 19, 3, -4, 13, -11, 15, 0, -1]
+const frutas = ['laranja', 'abacaxi', 'maçã', 'uva', 'jabuticaba', 'maracuja']
+
+// Numeros negativos
+const negativos = numeros.filter(n => n < 0)
+console.log('Números negativos:', negativos)
+
+// Números pares
+const pares = numeros.filter(x => x % 2 === 0)
+console.log('Números pares: ', pares)
+
+// Frutas que começam com a letra "m"
+const comecaComM = frutas.filter(f => f.charAt(0) === 'm')
+console.log('Frutas começando com a letra "m": ', comecaComM )
+    
+// Frutas que terminam com a letra "i"
+const terminaComI = frutas.filter(f => f.slice(-1) === 'i')
+console.log('Frutas terminando com a letra "i": ', terminaComI )
+
+// Frutas que terminam com a letra ""r
+const terminaComR = frutas.filter(f => f.slice(-1) === 'r')
+console.log('Frutas terminando com a letra "r": ', terminaComR )
