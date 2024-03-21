c6795a78d3e3a74546ff5ddb760d5a6a2bd4932f
ElianeO <elianecristina_d@yahoo.com>
2024-03-21 10:53:41 -0300

    (21/03) Criação do projeto react-hooks e primeiros exercícios

---
+++ react-hooks/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import Exercicio01 from './exercicios/01'
+import Exercício02 from './exercicios/02'
+
+function App() {
+
+  return (
+    <>
+     <h1> Exercícios React Hooks </h1>
+     <BrowserRouter>
+      <ul>
+        <li> <Link to="/01"> Exercício 01</Link> </li>
+        <li> <Link to="/02"> Exercício 02</Link> </li>
+      </ul>
+      <hr />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/01" element={<Exercicio01/>} />
+          <Route path="/02" element={<Exercício02/>} />
+        </Routes> 
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
