8c3d63d70602990d40053120d632eb12ac8f764f
ElianeO <elianecristina_d@yahoo.com>
2024-04-09 08:06:44 -0300

    (04/04) Término do projeto react-hooks e início do projeto karangos

--- react-hooks/src/exercicios/06.jsx
+++ react-hooks/src/exercicios/06.jsx
@@ -4,54 +4,96 @@ import * as React from 'react'
 // PokemonInfoFallback: o que é exibido enquanto as informações do pokémon
 // são carregadas
 // PokemonDataView: o componente usado para exibir as informações do pokémon
-import { fetchPokemon, PokemonInfoFallback, PokemonDataView, PokemonForm} from '../pokemon'
+import { fetchPokemon, PokemonInfoFallback, PokemonDataView, PokemonForm } from '../pokemon'
 
-function PokemonInfo({pokemonName}) {
+function PokemonInfo({ pokemonName }) {
   // 🐨 crie o estado para o pokémon (null)
-  const [pokemon, setPokemon] = React.useState(null)
-  const [error, setError] = React.useState(null)
+  // const [pokemon, setPokemon] = React.useState(null)
+  // const [error, setError] = React.useState(null)
+  // const [status, setStatus] = React.useState('idle')
+
+  // Criando uma única variável de estado, do tipo objeto, com a mesma funcionalidade de três variáveis de estado avulsas
+  const [state, setState] = React.useState({
+    pokemon: null,
+    error: null,
+    staus: 'idle'
+  })
+
+  // Usando desestruturação para obter versões somente-leitura das propriedades da variável de estado
+  const { pokemon, error, status } = state
 
   // 🐨 crie React.useEffect de modo a ser chamado sempre que pokemonName mudar.
   // 💰 NÃO SE ESQUEÇA DO VETOR DE DEPENDÊNCIAS!
   React.useEffect(() => {
     // 💰 se pokemonName é falso (ou uma string vazia) não se preocupe em fazer 
-  // a requisição (retorne precocemente).
-  if( !pokemonName) return
+    // a requisição (retorne precocemente).
+    if (!pokemonName) return
 
-  // 🐨 antes de chamar `fetchPokemon`, limpe o estado atual do pokemon
-  // ajustando-o para null.
-  setPokemon(null)
-  setError(null)
+    // 🐨 antes de chamar `fetchPokemon`, limpe o estado atual do pokemon
+    // ajustando-o para null.
+    // setPokemon(null)
+    // setError(null)
+    // setStatus('pending')
+    setState({ pokemon: null, error: null, staus: 'pending' })
 
-  // (Isso é para habilitar o estado de carregamento ao alternar entre diferentes
-  // pokémon.)
-  // 💰 Use a função `fetchPokemon` para buscar um pokémon pelo seu nome:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* atualize todos os estados aqui */},
-  //   )
-  fetchPokemon(pokemonName)
-    .then( // Callback de sucesso
-    pokemonData => setPokemon(pokemonData)
-  )
-  .catch( // Callback de falha
-    error => setError(error)
-  )
-}, [pokemonName])
+    // (Isso é para habilitar o estado de carregamento ao alternar entre diferentes
+    // pokémon.)
+    // 💰 Use a função `fetchPokemon` para buscar um pokémon pelo seu nome:
+    //   fetchPokemon('Pikachu').then(
+    //     pokemonData => {/* atualize todos os estados aqui */},
+    //   )
+    fetchPokemon(pokemonName)
+      .then( // Callback de sucesso
+        pokemonData => {
+          // setPokemon(pokemonData)
+          // setStatus('resolved')
+          setState({...state, pokemon: pokemonData, staus: 'resolved' })
+        }
+      )
+      .catch( // Callback de falha
+        error => {
+          // setError(error)
+          // setStatus('rejected')
+          setState({...state, error, staus: 'rejected '})
+        }
+      )
+  }, [pokemonName])
+
+  React.useEffect(() => {
+    console.count('Componente atualizou')
+  }, [])
   // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
   // 🐨 retorne o seguinte baseado nos estados `pokemon` e `pokemonName`:
   //   1. não há pokemonName: 'Informe um pokémon'
   //   2. tem pokemonName mas não pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. tem pokemon: <PokemonDataView pokemon={pokemon} />
-  if(error) return (
-    <div role="alert">
-      Houve um erro: <pre style={{ whiteSpace: 'normal'}} > {error.message} </pre>
-    </div>
-  )
-  if( !pokemonName) return 'Informe um pokemon'
-  if(pokemonName && ! pokemon) return <PokemonInfoFallback nome={pokemonName} />
-  if(pokemon) return <PokemonDataView pokemon={pokemon} />
-  
+  // if(error) return (
+  //   <div role="alert">
+  //     Houve um erro: <pre style={{ whiteSpace: 'normal'}} > {error.message} </pre>
+  //   </div>
+  // )
+  // if( !pokemonName) return 'Informe um pokemon'
+  // if(pokemonName && ! pokemon) return <PokemonInfoFallback nome={pokemonName} />
+  // if(pokemon) return <PokemonDataView pokemon={pokemon} />
+
+  switch (status) {
+    case 'idle':
+      return 'Informe um Pokemon'
+
+    case 'pending':
+      return <PokemonInfoFallback name={pokemonName} />
+
+    case 'resolved':
+      return <PokemonDataView pokemon={pokemon} />
+
+    default: // 'rejected'
+      return (
+        <div role="alert">
+          Houve um erro: <pre style={{ whiteSpace: 'normal' }}> {error.message} </pre>
+        </div>
+      )
   }
+}
 
 
 
